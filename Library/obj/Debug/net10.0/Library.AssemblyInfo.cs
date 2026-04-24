53573f2cf3624d23da4346d3722c91d314bb5564
Iris-Healy <kl3224zr@go.minnstate.edu>
2026-04-24 10:19:15 +0000

    Fixed Role Based Authentication

--- Library/obj/Debug/net10.0/Library.AssemblyInfo.cs
+++ Library/obj/Debug/net10.0/Library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c2fef795b3d3450a8a492c8dba5c2b5b33d9f0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4506f21414229fa8367b01d1e9d24d4c4af3cd0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
